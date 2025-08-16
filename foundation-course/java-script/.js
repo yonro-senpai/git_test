ed528409271feef8fb650310f6a7decd4fd02657
yonro-senpai <224741379+yonro-senpai@users.noreply.github.com>
2025-08-16 22:19:04 +0530

    Practice & Complete exercise of javascript foundation lesson 1
    Completed Practice question of javascript, also worked along with
    reading and completed assignment as well.

---
+++ foundation-course/java-script/.js
@@ -0,0 +1,17 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+
+pi = 10;
+
+console.log(pi)
